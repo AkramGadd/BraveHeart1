7f89c61e0e567e3108360217132ed1e00cfda707
AkramGadd <147104607+AkramGadd@users.noreply.github.com>
2025-05-01 11:48:30 +0300

    final commit

--- 2D platformer/Assets/Scripts/Damageable.cs
+++ 2D platformer/Assets/Scripts/Damageable.cs
@@ -6,6 +6,7 @@ using UnityEngine.Events;
 public class Damageable : MonoBehaviour
 {
     public UnityEvent<int, Vector2> damageableHit;
+    public UnityEvent<int, int> healthChanged;
 
     Animator animator;
 
@@ -57,6 +58,7 @@ public class Damageable : MonoBehaviour
         set
         {
             _health = value;
+            healthChanged?.Invoke(Health, MaxHealth);
 
             if (_health <= 0 && !IsAlive)
             {
